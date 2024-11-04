4eb0e063c9f2d6093c6bfe8e52ed8657221d3cac
Alejandro-Jaime-Pozas <ajaimepozas@gmail.com>
2024-11-04 09:57:14 -0600

    s

--- personal/wb.py
+++ personal/wb.py
@@ -3,33 +3,33 @@ from typing import Optional, List
 # INCLUDE THIS ALWAYS!!!
 
 
-# 123. Best Time to Buy and Sell Stock III
-# You are given an array prices where prices[i] is the price of a given stock on the ith day.
-# Find the maximum profit you can achieve. You may complete at most two transactions.
-# Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
-# Example 1:
-# Input: prices = [3,3,5,0,0,3,1,4]
-# Output: 6
-# Explanation: Buy on day 4 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
-# Then buy on day 7 (price = 1) and sell on day 8 (price = 4), profit = 4-1 = 3.
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        # what i would do personally, is check all trough to peaks (meaning lowest to highest) and get their values as possible profit values, then compare them with each other depending on their index values to see which pairs are both highest profit and compatible in range
-        # if holding stock (bought) you cannot buy another, must sell prior
-        # you can either never buy a stock, buy and sell once, or buy and sell twice
-        # if you buy and sell twice, cannot hold 2 at once, must sell prior
-        # cannot sell then buy on same day (should be no reason to)
-        # could be O(2n) problem, you just run through list twice to check best?
-        # keep track if holding stock, cannot buy while holding
-        # multiple possible options, so need to compare all options
-        # for each num, check if buy that num, what is max profit achievable in 1 or 2 buy/sell actions from that num forward
-        # no overlap so grab the two highest profits
-        highest1 = 0
-        highest2 = 0
-        for i in range(len(prices)-1):
-            if prices[i+1] > highest1
+
+
+
+# # 123. Best Time to Buy and Sell Stock III
+# # You are given an array prices where prices[i] is the price of a given stock on the ith day.
+# # Find the maximum profit you can achieve. You may complete at most two transactions.
+# # Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+# # Example 1:
+# # Input: prices = [3,3,5,0,0,3,1,4]
+# # Output: 6
+# # Explanation: Buy on day 4 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3.
+# # Then buy on day 7 (price = 1) and sell on day 8 (price = 4), profit = 4-1 = 3.
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         # what i would do personally, is check all trough to peaks (meaning lowest to highest) and get their values as possible profit values, then compare them with each other depending on their index values to see which pairs are both highest profit and compatible in range
+#         # if holding stock (bought) you cannot buy another, must sell prior
+#         # you can either never buy a stock, buy and sell once, or buy and sell twice
+#         # if you buy and sell twice, cannot hold 2 at once, must sell prior
+#         # cannot sell then buy on same day (should be no reason to)
+#         # could be O(2n) problem, you just run through list twice to check best?
+#         # keep track if holding stock, cannot buy while holding
+#         # multiple possible options, so need to compare all options
+#         # for each num, check if buy that num, what is max profit achievable in 1 or 2 buy/sell actions from that num forward
+#         # no overlap so grab the two highest profits
+
     
-print(Solution().maxProfit([3,3,5,0,0,3,1,4]))
+# print(Solution().maxProfit([3,3,5,0,0,3,1,4]))
         
 
 # # best time to buy and sell stock II
