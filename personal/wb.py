115897034f0b4b7913537b50b3ac9e4d187ca91d
Alejandro-Jaime-Pozas <ajaimepozas@gmail.com>
2024-12-05 09:24:11 -0600

    s

--- personal/wb.py
+++ personal/wb.py
@@ -4,6 +4,17 @@ from typing import Optional, List
 
 
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        
+
+
 
 
 # # 142. Linked List Cycle II
