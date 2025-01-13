dc97956bd7ec28d7828a128d695275984ad6f7fa
Alejandro-Jaime-Pozas <ajaimepozas@gmail.com>
2025-01-13 09:38:16 -0600

    postorder trav bin tree

--- personal/wb.py
+++ personal/wb.py
@@ -3,37 +3,50 @@ from typing import Optional, List
 # INCLUDE THIS ALWAYS!!!
 
 
-# Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+# 160. Intersection of Two Linked Lists
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
 class Solution:
-    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        # post is leftmost leaf first, then right, then move upwards
-        final = []
-
-        def dfs(root):
-            if not root:
-                return 
-            # first visit left, then right 
-            dfs(root.left)
-            dfs(root.right)
-            final.append(root.val)
-
-        dfs(root)
-        return final 
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        
 
 
-root = TreeNode(1)
-root.left = TreeNode(2)
-root.left.left = TreeNode(4)
-root.left.right = TreeNode(5)
-root.right = TreeNode(8)
+
+# # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+# class Solution:
+#     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+#         # post is leftmost leaf first, then right, then move upwards
+#         final = []
+
+#         def dfs(root):
+#             if not root:
+#                 return 
+#             # first visit left, then right 
+#             dfs(root.left)
+#             dfs(root.right)
+#             final.append(root.val)
+
+#         dfs(root)
+#         return final 
 
 
-print(Solution().postorderTraversal(root))
+# root = TreeNode(1)
+# root.left = TreeNode(2)
+# root.left.left = TreeNode(4)
+# root.left.right = TreeNode(5)
+# root.right = TreeNode(8)
+
+
+# print(Solution().postorderTraversal(root))
 
 
 # # Definition for a binary tree node.
