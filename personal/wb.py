b766ebade1f08e143dd115872654d96933fadb8d
Alejandro-Jaime-Pozas <ajaimepozas@gmail.com>
2025-01-22 09:21:21 -0600

    candies

--- personal/wb.py
+++ personal/wb.py
@@ -3,7 +3,17 @@ from typing import Optional, List
 # INCLUDE THIS ALWAYS!!!
 
 
-
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        greatest = max(candies)
+        for i in range(len(candies)):
+            if candies[i] + extraCandies >= greatest:
+                candies[i] = True
+            else:
+                candies[i] = False
+        return candies 
+    
+print(Solution().kidsWithCandies([2,3,5,1,3], 3))
 
 
 
