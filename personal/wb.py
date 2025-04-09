ce66bc8c8b47e3152d0c4ab35c193724d646827d
Alex <ajaimepozas@gmail.com>
2025-04-09 10:41:17 -0600

    s

--- personal/wb.py
+++ personal/wb.py
@@ -4,8 +4,84 @@ from collections import Counter, deque
 # INCLUDE THIS ALWAYS!!!
 
 
+# 394. Decode String
+class Solution:
+    def decodeString(self, s: str) -> str:
+        # brackets will indicate string to repeat, number to left of bracket indicates exact
+        # there can be nested brackets
+        # number will ALWAYS preceed opening bracket
+        # something like if c is digit, if c is open/close bracket, if c is letter
+        # will need a stack for nested brackets
+        # thinking of separating stacks, one for the number, one for the chars and multiply
+        # will need to resolve nested bracket strs first, to calculate outer ones
+        # [3, 2]
+        # [a, c]
+        # multiply number * character, then add prior character to that string, then multiply again
+        # whenever there's equal open to close brackets, trigger multipying stacks and empty stacks
 
+        # Use a stack to handle nested brackets and repeated substrings
+        stack = []
+        current_string = ""
+        current_number = 0
 
+        for char in s:
+            if char.isdigit():
+                # Build the current number (handles multi-digit numbers)
+                current_number = current_number * 10 + int(char)
+            elif char == '[':
+                # Push the current string and number onto the stack
+                stack.append((current_string, current_number))
+                # Reset for the new context inside the brackets
+                current_string = ""
+                current_number = 0
+            elif char == ']':
+                # Pop from the stack and repeat the current string
+                last_string, repeat_count = stack.pop()
+                current_string = last_string + current_string * repeat_count
+            else:
+                # Append the current character to the current string
+                current_string += char
+
+        return current_string
+
+# Example usage
+print(Solution().decodeString('2[abc]3[cd]ef'))  # Output: "abcabccdcdcdef"
+
+        # final = ''
+        # alphas = ''
+        # numbers = []
+        # chars = []
+        # brackets = 'none yet'
+        # for i, c in enumerate(s):
+        #     if c.isdigit():
+        #         numbers.append(int(c))
+        #     elif c == '[':
+        #         if brackets == 'none yet':
+        #             brackets = 1
+        #         else:
+        #             brackets += 1
+        #     elif c == ']':
+        #         brackets -= 1
+        #     elif c.isalpha():
+        #         # print(c)
+        #         alphas += c
+        #         # if next c is NOT alpha, append and reset alphas
+        #         if i+1 < len(s)-1 and not s[i+1].isalpha():
+        #             # print(s[i+1])
+        #             chars.append(alphas)
+        #             alphas = ''
+        #         elif i+1 == len(s)-1:
+        #             final += alphas
+        #     # if bracket closed, trigger multiplication and empty stacks
+        #     if brackets == 0:
+        #         while numbers and chars:
+        #             print(numbers, chars)
+        #             concat = numbers.pop() * chars.pop()
+        #             # if prev chars, means need to join prev char to concat
+        #             if chars:
+        #                 chars[-1] = chars[-1] + concat
+        #         final += concat
+        # return final
 
 
 # # 735. Asteroid Collision
